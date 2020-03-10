54536352b1c1310a29c9854e9ce03792c0e24380
Janez Cimerman <j.cimko@gmail.com>
2020-03-10 15:45:33 +0100

    Ate is now being logged into external file for excel processing

--- mrntc_ref_ws/src/ate_calculator/src/ate_calculator_node.cpp
+++ mrntc_ref_ws/src/ate_calculator/src/ate_calculator_node.cpp
@@ -1,6 +1,11 @@
+// ATE = absolute trajectory error
+
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <iostream>
+#include <fstream>
+
 
 using namespace std;
 
@@ -9,6 +14,8 @@ int main(int argc, char** argv){
 
   ros::NodeHandle node;
 
+
+  //#####################################PARAMS################################################################### 
   string reference_target_frame;
   string test_target_frame;
   string reference_source_frame;
@@ -17,15 +24,44 @@ int main(int argc, char** argv){
   node.param<std::string>("/ate_calculator/test_souce_frame", test_source_frame, "test_s_frame");
   node.param<std::string>("/ate_calculator/reference_source_frame", reference_source_frame, "ref_s_frame");
   node.param<std::string>("/ate_calculator/test_target_frame", test_target_frame, "test_t_frame");
+  
+  double timestamp_time_difference_thresh; // seconds
+  node.param("/ate_calculator/timestamp_time_difference_thresh", timestamp_time_difference_thresh, 0.01);
+
+  string out_file;
+  node.param<std::string>("/ate_calculator/out_file_path", out_file, "output.txt");
+ 
+  //#############################################################################################################
+
 
   ROS_INFO("Comparing transformation %s -> %s \n with transformation %s -> %s", reference_source_frame.c_str(),
                                                                                 reference_target_frame.c_str(),
                                                                                 test_source_frame.c_str(),
                                                                                 test_target_frame.c_str());
 
+  ROS_INFO("Timestamp time difference thresh is set to %f", timestamp_time_difference_thresh);                                                                                
+  ROS_INFO("Writing results to file: %s", out_file.c_str());                                                                                
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
 
+  //logging
+  // ofstream myfile("output.txt"); //src/ate_calculator/src/
+  
+  
+  // try{
+  //   myfile.open("output.txt", std::ios::trunc);
+  // }
+  // catch (const std::exception& e) {
+  //   ROS_WARN("error opening the logging file");
+  //   return 0;
+  // } 
+
+  ofstream myfile(out_file.c_str());
+
+  string output_text;
+  double dx, dy, dist_diff;
+
+  // main loop
   ros::Rate rate(10.0);
   while (node.ok()){
     geometry_msgs::TransformStamped refTransform;
@@ -39,8 +75,6 @@ int main(int argc, char** argv){
       continue;
     }
 
-    // ROS_INFO("REF: x: %lf, y %lf", refTransform.transform.translation.x, refTransform.transform.translation.y);
-
     try{
       testTransform = tfBuffer.lookupTransform(test_target_frame, test_source_frame, ros::Time(0)); //target_frame = child_frame, source_frame = parent_frame
     }
@@ -52,15 +86,27 @@ int main(int argc, char** argv){
 
     // ROS_INFO("TEST: x: %lf, y %lf", testTransform.transform.translation.x, testTransform.transform.translation.y);
 
-    double dx = fabs(refTransform.transform.translation.x-testTransform.transform.translation.x);
-    double dy = fabs(refTransform.transform.translation.y-testTransform.transform.translation.y);
+    // check the timestamp difference between the two messages
+    ros::Duration diff=testTransform.header.stamp-refTransform.header.stamp;
+    if(fabs(diff.toSec())>timestamp_time_difference_thresh){
+      ROS_WARN("There are timestamp differences between tranformations larger than %f. Not counting towards ATE anymore", timestamp_time_difference_thresh);
+    }
+    else{
+      dx = fabs(refTransform.transform.translation.x-testTransform.transform.translation.x);
+      dy = fabs(refTransform.transform.translation.y-testTransform.transform.translation.y);
+      dist_diff = sqrt(dx*dx+dy*dy);
 
-    double ate = sqrt(dx*dx+dy*dy);
-
-    ROS_INFO("ATE: %lf", ate);
-
+      
+      // logging difference and 
+      myfile << dist_diff << "\t";
+      myfile << testTransform.header.stamp;
+      myfile << endl;
+    }
 
     rate.sleep();
   }
+
+  myfile.close();
+  
   return 0;
 };
